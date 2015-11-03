847d1bda377f9fa55791def11de7efc45aa91b8a
tomfun <tomfun1990@gmail.com>
2015-11-03 11:04:58 +0200

    security

--- app.js
+++ app.js
@@ -19,22 +19,80 @@ var Twig = require('twig');// Twig module
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
-app.set('debug', app.get('env') === 'development'||  app.get('env') === 'dev');
+app.set('debug', app.get('env') === 'development' || app.get('env') === 'dev');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var unsecureUrls = [
+    {
+        method: 'post',
+        url: '/api/register',
+    },
+    {
+        method: 'post',
+        url: '/api/login',
+    },
+    {
+        method: 'get',
+        url: '/',//search users,
+    },
+    {
+        method: 'get',
+        url: '/api/user',//search users
+        query: true,
+    },
+    {
+        method: 'get',
+        url: '/api/item',
+        query: true,//search goods
+    },
+    {
+        method: 'get',
+        url: '/api/item/',// must start with "/api/item/"
+        params: true, // and may ends with some
+        query: false, // and can't include ?...
+    },
+];
+app.use(function (req, res, next) {
+    var token = req.get('Authorization');
+    if (!token) {
+
+        var _ = require('lodash');
+        var urlData = req._parsedOriginalUrl || req._parsedUrl;
+        var isOk = _.any(_.filter(unsecureUrls, function(v) {
+            var m = v.method;
+            return !m || (m = String(m).toUpperCase()) === req.method || m === 'ANY';
+        }), function (v) {
+            if (v.url === urlData.pathname) {
+                return !urlData.search || (v.query === true && urlData.search) || (v.query === urlData.search);
+            }
+            if (urlData.pathname.indexOf(v.url) === 0) {
+                return v.params === true;
+            }
+            return false;
+        });
+        if (isOk) {
+            return next();
+        }
+        //console.log(req.originalUrl, req.params, req.query, req._parsedOriginalUrl);
+        res.status(401).send();
+    } else {
+        console.log(token);
+        return next();
+    }
+});
 app.use('/', routes);
 app.use('/api', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -42,23 +100,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error:   err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error:   {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -11,19 +11,21 @@ var fingerTip = String(router);
 /* GET home page. */
 router.get('/', function (req, res, next) {
     if (app.get('debug')) {
-        res.render("console", {stack: _.map(app._router.stack, function (v) {
-            var source = String(v.handle),
-                res = {
-                'name': String(v.name),
-                'handle': source,
-                'itself': v.handle === router,
-                'my': fingerTip === source,
-                'regexp': String(v.regexp),
-                'descr': v.handle.descr
-            };
-            //console.log(v);
-            return res;
-        })});
+        res.render("console", {
+            stack: _.map(app._router.stack, function (v) {
+                var source = String(v.handle),
+                    res    = {
+                        'name':   String(v.name),
+                        'handle': source,
+                        'itself': v.handle === router,
+                        'my':     fingerTip === source,
+                        'regexp': String(v.regexp),
+                        'descr':  v.handle.descr
+                    };
+                //console.log(v);
+                return res;
+            })
+        });
     } else {
         next();
     }
